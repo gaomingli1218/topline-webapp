ef6a91d29a9955eb8165c5775aa48c9d4d5d4a80
gaomingli <gaomingli@qq.com>
2019-09-20 19:22:09 +0800

    登录页面功能完成

--- src/main.js
+++ src/main.js
@@ -2,9 +2,51 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import { Button } from 'vant'
+import './styles/login.less'
+import { ValidationProvider, ValidationObserver, extend } from 'vee-validate'
+import zhCN from 'vee-validate/dist/locale/zh_CN' // 加载验证插件的语言包
+import * as rules from 'vee-validate/dist/rules'
+import {
+  NavBar,
+  CellGroup,
+  Field,
+  Button,
+  Loading,
+  Toast
+} from 'vant'
 
-Vue.use(Button)
+Vue
+  .use(NavBar)
+  .use(CellGroup)
+  .use(Field)
+  .use(Button)
+  .use(Loading)
+  .use(Toast)
+
+// 配置使用中文语言
+for (let rule in rules) {
+  extend(rule, {
+    ...rules[rule], // add the rule
+    message: zhCN.messages[rule] // add its message
+  })
+}
+// 自定义验证必须是是手机号
+extend('phone', {
+  validate (value) {
+    return /^1\d{10}$/.test(value)
+  },
+  message: '必须是手机号'
+})
+extend('code', {
+  validate (value) {
+    return /^\d{6}$/.test(value)
+  },
+  message: '必须是6位验证码'
+})
+
+// 注册为全局组件
+Vue.component('ValidationProvider', ValidationProvider)
+Vue.component('ValidationObserver', ValidationObserver)
 
 Vue.config.productionTip = false
 
---
+++ src/views/login/index.vue
@@ -0,0 +1,83 @@
+<template>
+  <div>
+    <van-nav-bar title="登录" />
+    <!--
+      ValidationObserver 组件会渲染成一个form表单
+      可以通过 ref 调用这个组件的方法：validate 来进行js验证
+    -->
+    <ValidationObserver tag="form" ref="loginForm">
+      <!--
+        把需要验证的字段用 ValidationProvider 包裹起来
+        在其上面配置对应的验证规则等信息
+      -->
+      <ValidationProvider tag="div" name="手机号" rules="required|phone" v-slot="{ errors }">
+        <van-field
+          v-model="user.mobile"
+          required
+          clearable
+          label="手机"
+          placeholder="请输入手机号"
+          :error-message="errors[0]"
+        />
+      </ValidationProvider>
+      <ValidationProvider tag="div" name="验证码" rules="required|code" v-slot="{ errors }">
+        <van-field
+          v-model="user.code"
+          label="验证码"
+          placeholder="请输入验证码"
+          required
+          :error-message="errors[0]"
+        />
+      </ValidationProvider>
+    </ValidationObserver>
+    <div class="login-btn">
+      <van-button type="info" :loading="isLoading" @click="onLogin">登录</van-button>
+    </div>
+  </div>
+</template>
+
+<script>
+// import request from "@/utils/request";
+import { login } from '@/api/user'
+export default {
+  data () {
+    return {
+      user: {
+        mobile: '',
+        code: ''
+      },
+      isLoading: false
+    }
+  },
+  methods: {
+    async onLogin () {
+      // 发送请求
+      try {
+        const isValid = await this.$refs.loginForm.validate()
+        if (!isValid) {
+          return
+        }
+        this.isLoading = true
+        const { data } = await login(this.user)
+        console.log(data)
+        this.$toast.success('登录成功')
+        this.isLoading = false
+      } catch (err) {
+        if (err.response && err.response.status === 400) {
+          this.$toast.fail('手机号或验证码错误')
+        }
+      }
+      this.isLoading = false
+    }
+  }
+}
+</script>
+
+<style lang='less' scoped>
+.login-btn {
+  padding: 20px;
+  .van-button {
+    width: 100%;
+  }
+}
+</style>
